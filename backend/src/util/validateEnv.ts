af208cb58cd0501488b0ecf01a26c4c790509e67
benjaminjohnson2204 <ben332004@gmail.com>
2023-11-25 18:14:48 -0800

    Add email user/password to backend

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -4,10 +4,12 @@
  */
 
 import { cleanEnv } from "envalid";
-import { port, str } from "envalid/dist/validators";
+import { port, str, email } from "envalid/dist/validators";
 
 export default cleanEnv(process.env, {
-  PORT: port(),
-  MONGODB_URI: str(),
-  FRONTEND_ORIGIN: str(),
+  PORT: port(), // Port to run backend on
+  MONGODB_URI: str(), // URI of MongoDB database to use
+  FRONTEND_ORIGIN: str(), // URL of frontend, to allow CORS from frontend
+  EMAIL_USER: email(), // Email address to use for sending emails
+  EMAIL_APP_PASSWORD: str(), // App password to use for sending emails
 });
