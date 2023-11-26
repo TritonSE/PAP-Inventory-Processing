4263441633d0e588268df5d762f23e9df314af20
benjaminjohnson2204 <ben332004@gmail.com>
2023-11-25 17:41:24 -0800

    Set up backend, husky, linter & prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,84 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [".eslintrc.cjs", "webpack.config.js", "jest.config.cjs"],
+  plugins: ["@typescript-eslint", "react", "no-relative-import-paths"],
+  rules: {
+    "default-case": "off",
+    "no-plusplus": "off",
+    "no-continue": "off",
+    "prefer-template": "off",
+    "prefer-destructuring": "off",
+    "class-methods-use-this": "off",
+
+    /**
+     * Don't require "React" to be in scope when using JSX because
+     * NextJS automatically imports React for us
+     */
+    "react/react-in-jsx-scope": "off",
+
+    // Allow leading underscores in identifiers (e.g. _id in MongoDB).
+    "no-underscore-dangle": "off",
+
+    // Some APIs use snake_case identifiers.
+    camelcase: "off",
+
+    // Depending on the context, using bracket notation might be clearer.
+    "dot-notation": "off",
+
+    /**
+     * Reassigning parameters can be useful to avoid creating another variable,
+     * and to modify objects by reference.
+     */
+    "no-param-reassign": "off",
+
+    /**
+     * Unused variables and arguments should be removed in most cases, but it's
+     * convenient to allow them when the code is still being implemented.
+     *
+     * Prefix variable names with an underscore to suppress the warning.
+     *
+     * We disable the standard ESLint version of this rule and use the
+     * TypeScript version instead. This handles things like function type
+     * declarations better.
+     */
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+
+    // Not necessary for some APIs (consistency reasons)
+    "import/prefer-default-export": "off",
+
+    // Stylistic rules.
+    "lines-between-class-members": "off",
+
+    "no-relative-import-paths/no-relative-import-paths": [
+      "warn",
+      { allowSameFolder: false, rootDir: "frontend" },
+    ],
+  },
+};
