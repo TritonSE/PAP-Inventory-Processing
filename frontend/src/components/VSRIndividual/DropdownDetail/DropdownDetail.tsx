a0d27b6256c4574ffdb6babe8c860851f887aa0c
benjaminjohnson2204 <ben332004@gmail.com>
2024-02-11 00:53:22 -0800

    Minor styling fixes to status dropdown

--- frontend/src/components/VSRIndividual/DropdownDetail/DropdownDetail.tsx
+++ frontend/src/components/VSRIndividual/DropdownDetail/DropdownDetail.tsx
@@ -12,7 +12,19 @@ export interface DropdownDetailProps {
 
 export function DropdownDetail({ title, value }: DropdownDetailProps) {
   const DropdownIcon = () => (
-    <Image src="/keyboard_arrow_down24.svg" width={24} height={24} alt="dropdown" />
+    <Image
+      src="/keyboard_arrow_down24.svg"
+      width={24}
+      height={24}
+      alt="dropdown"
+      style={{
+        // We need these styles in order to make our custom IconComponent clickable.
+        // See https://stackoverflow.com/a/73038614
+        position: "absolute",
+        right: 7,
+        pointerEvents: "none",
+      }}
+    />
   );
 
   const [selectedValue, setSelectedValue] = useState(value);
@@ -35,35 +47,67 @@ export function DropdownDetail({ title, value }: DropdownDetailProps) {
               boxShadow: "none",
               ".MuiOutlinedInput-notchedOutline": { border: 0 },
               ".MuiOutlinedInput-input.MuiSelect-select": {
-                paddingLeft: "0px",
-                paddingRight: "0px",
-                paddingTop: "0px",
-                paddingBottom: "0px",
-                ":first-of-type": { paddingRight: "8px" },
+                padding: "0px",
+
+                // This right padding keeps the dropdown arrow from being
+                // colored in like the menu itmes
+                ":first-of-type": { paddingRight: "32px" },
+              },
+
+              // Override default blue border on Select component when focused
+              // See https://stackoverflow.com/a/75937349
+              fieldset: {
+                border: "none !important",
               },
             }}
             MenuProps={{
               disableScrollLock: true,
+              // Left-align menu items with Select component
+              // See https://stackoverflow.com/a/64932005
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left",
+              },
+              transformOrigin: {
+                vertical: "top",
+                horizontal: "left",
+              },
+
+              style: {
+                // Add vertical gap between Select component and its menu items
+                marginTop: "8px",
+              },
+
+              sx: {
+                // Set padding on each menu item
+                ".MuiMenuItem-root": {
+                  padding: "12px",
+                },
+              },
             }}
             IconComponent={DropdownIcon}
           >
             <MenuItem value={"Received"}>
-              <div className={styles.received}>Received</div>
+              <div className={`${styles.statusOption} ${styles.received}`}>Received</div>
             </MenuItem>
             <MenuItem value={"Appointment Schedule"}>
-              <div className={styles.scheduled}>Appointment Scheduled</div>
+              <div className={`${styles.statusOption} ${styles.scheduled}`}>
+                Appointment Scheduled
+              </div>
             </MenuItem>
             <MenuItem value={"Approved"}>
-              <div className={styles.approved}>Approved</div>
+              <div className={`${styles.statusOption} ${styles.approved}`}>Approved</div>
             </MenuItem>
             <MenuItem value={"Resubmit"}>
-              <div className={styles.resubmit}>Resubmit</div>
+              <div className={`${styles.statusOption} ${styles.resubmit}`}>Resubmit</div>
             </MenuItem>
             <MenuItem value={"No-show / Incomplete"}>
-              <div className={styles.incomplete}>No-show / Incomplete</div>
+              <div className={`${styles.statusOption} ${styles.incomplete}`}>
+                No-show / Incomplete
+              </div>
             </MenuItem>
             <MenuItem value={"Archived"}>
-              <div className={styles.archived}>Archived</div>
+              <div className={`${styles.statusOption} ${styles.archived}`}>Archived</div>
             </MenuItem>
           </Select>
         </FormControl>
