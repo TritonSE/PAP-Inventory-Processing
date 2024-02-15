b06cf2cd3e776479599a28c49fb07523b6afb581
Harsh Gurnani <harshgurnani@Harshs-MacBook-Pro.local>
2024-02-15 07:36:05 -0800

    copied over vsr model and created backend delete route

--- backend/src/routes/vsr.ts
+++ backend/src/routes/vsr.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
+import { AuthError } from "src/errors/auth";
+import { ServiceError } from "src/errors/service";
 import { verifyAuthToken } from "src/middleware/auth";
+import { User } from "src/models/user";
+import VSRModel from "src/models/vsr";
 
 const router = express.Router();
 
@@ -8,9 +12,22 @@ router.delete(
   [verifyAuthToken],
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const vsrId = req.params.id;
       const uid = req.body.uid;
-      console.log(vsrId, uid);
+      const user = await User.findOne({ uid: uid });
+      if (!user) {
+        throw ServiceError.USER_NOT_FOUND;
+      }
+
+      const { role } = user;
+      if (role != "admin") {
+        throw AuthError.NOT_ADMIN;
+      }
+
+      const vsrId = req.params.id;
+      await VSRModel.findByIdAndDelete(vsrId);
+      return res.status(200).json({
+        message: `successfully deleted vsr ${vsrId}`,
+      });
     } catch (e) {
       next();
     }
