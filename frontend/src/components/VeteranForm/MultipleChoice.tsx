fd7fe782365f9e5b97f8d3539ca723fabbb618a6
Syu125 <yusophia2011@gmail.com>
2024-02-21 18:50:57 -0800

    Create furniture json (uploaded to mongodb), imported existing components

---
+++ frontend/src/components/VeteranForm/MultipleChoice.tsx
@@ -0,0 +1,52 @@
+import Chip from "@mui/material/Chip";
+import styles from "@/components/MultipleChoice.module.css";
+
+export interface MultipleChoiceProps {
+  label: string;
+  options: string[];
+  value: string;
+  onChange: (selected: string) => void;
+  required: boolean;
+  error?: boolean;
+  helperText?: string;
+}
+
+const MultipleChoice = ({
+  label,
+  options,
+  value,
+  onChange,
+  required,
+  helperText,
+}: MultipleChoiceProps) => {
+  return (
+    <div className={styles.wrapperClass}>
+      <p>
+        {required && <span className={styles.requiredAsterisk}>* </span>}
+        {label}
+      </p>
+      <div className={styles.chipContainer}>
+        {options.map((option) => (
+          <Chip
+            label={option}
+            key={option}
+            onClick={() => {
+              if (value === option) {
+                onChange("");
+              } else {
+                onChange(option);
+              }
+            }}
+            className={`${styles.chip} ${
+              value === option ? styles.chipSelected : styles.chipUnselected
+            }`}
+            clickable
+          />
+        ))}
+      </div>
+      <div className={styles.helperText}>{helperText}</div>
+    </div>
+  );
+};
+
+export default MultipleChoice;
---
+++ frontend/src/components/VeteranForm/TextField.tsx
@@ -0,0 +1,38 @@
+import styles from "src/components/TextField.module.css";
+import MUITextField, { TextFieldProps as MUITextFieldProps } from "@mui/material/TextField";
+import { ForwardedRef, forwardRef } from "react";
+
+export interface TextFieldProps extends MUITextFieldProps<"outlined"> {
+  label: string;
+  error?: boolean;
+  helperText?: string;
+  required: boolean;
+}
+
+const TextField = forwardRef(
+  (
+    { label, error, required, helperText, ...props }: TextFieldProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    console.log(props);
+    return (
+      <div className={styles.wrapperClass}>
+        <p>
+          {required && <span className={styles.requiredAsterisk}>* </span>}
+          {label}
+        </p>
+        <MUITextField
+          ref={ref}
+          fullWidth={true}
+          size="small"
+          className={styles.inputClass}
+          error={error}
+          {...props}
+        />
+        <div className={styles.helperText}>{helperText}</div>
+      </div>
+    );
+  },
+);
+TextField.displayName = "TextField";
+export default TextField;
