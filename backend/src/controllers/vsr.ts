3750b8220cffa7a6567b3b350df7335a1832aafe
Steven Shi <57511106+2s2e@users.noreply.github.com>
2024-02-24 23:54:54 -0800

    Feature/steven+yoto vsr personal info (#24)
    * lint fixes
    
    * Added route to POST and VSR controller
    
    * Added validation functions for form responses
    
    * Added validation functions for form responses
    
    * Removed numOfBoys, numOfGirls from schema, cleaned up backend, renamed martial to marital
    
    * Added tests for validate spouse name
    
    * Added tests for validate ethnicity
    
    * Basic text field, multiple choice, dropdown, and submit
    
    * Fix linting issues and minor edit to gender field validation
    
    * app now uses post route
    
    * Attempt at linking front end with back end, onbooarding code for front end api requests imported and adjusted
    
    * Reworked TextField component to do ref-forwarding, successfully pushed a document to the mongo database
    
    * Fixed couple bugs with date verification
    
    * Removed forwardRef
    
    * Added title and description, required fields, and overall more styling
    
    * updated date to use current date, added more fields for the vsr page
    
    * Prototype of ethnicity selector
    
    * Made options unselectable
    
    * Working version of ethnicity form
    
    * Prototype of appearing and disappearing children form
    
    * Added more styling
    
    * Added backend functionality for ages, changed Child #n name to Child #n age
    
    * Fixed small bug with ethnicity, added numBoys and spouse to createVSR, and continued styling
    
    * fixed issue with ethnicity selection
    
    * Changed textbox styling to flex 1
    
    * Fixed issue with null values in age array
    
    * Page number component
    
    * added girls form
    
    * changes with validation
    
    * Yet even more lint fixes
    
    * Fix CSS styles & clean up code
    
    * Update favicon, title, & description, remove unused boilerplate
    
    * Remove commented-out code & make ethnicity an array
    
    * Fix bugs with entering ages of children
    
    * Only require spouse's name for Married marital status
    
    * Fix age validation
    
    * Fix bugs with VSR age & num children
    
    * Address Daniel's review comments
    
    ---------
    
    Co-authored-by: Yoto Kim <yongtokim1215@gmail.com>
    Co-authored-by: Benjamin Johnson <ben332004@gmail.com>

---
+++ backend/src/controllers/vsr.ts
@@ -0,0 +1,51 @@
+import { RequestHandler } from "express";
+import { validationResult } from "express-validator";
+import VSRModel from "src/models/vsr";
+import validationErrorParser from "src/util/validationErrorParser";
+
+export const createVSR: RequestHandler = async (req, res, next) => {
+  // extract any errors that were found by the validator
+  const errors = validationResult(req);
+  const {
+    name,
+    gender,
+    age,
+    maritalStatus,
+    spouseName,
+    agesOfBoys,
+    agesOfGirls,
+    ethnicity,
+    employmentStatus,
+    incomeLevel,
+    sizeOfHome,
+  } = req.body;
+
+  try {
+    // if there are errors, then this function throws an exception
+    validationErrorParser(errors);
+
+    // Get the current date as a timestamp for when VSR was submitted
+    const date = new Date();
+
+    const vsr = await VSRModel.create({
+      name,
+      date,
+      gender,
+      age,
+      maritalStatus,
+      spouseName,
+      agesOfBoys,
+      agesOfGirls,
+      ethnicity,
+      employmentStatus,
+      incomeLevel,
+      sizeOfHome,
+    });
+
+    // 201 means a new resource has been created successfully
+    // the newly created VSR is sent back to the user
+    res.status(201).json(vsr);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ frontend/src/components/TextField.tsx
@@ -0,0 +1,42 @@
+import styles from "src/components/TextField.module.css";
+import MUITextField, { TextFieldProps as MUITextFieldProps } from "@mui/material/TextField";
+import { ForwardedRef, forwardRef } from "react";
+
+export interface TextFieldProps extends MUITextFieldProps<"outlined"> {
+  label: string;
+  error?: boolean;
+  helperText?: string;
+  required: boolean;
+}
+
+const TextField = forwardRef(
+  (
+    { label, error, required, helperText, ...props }: TextFieldProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    return (
+      <div className={styles.wrapperClass}>
+        <p>
+          {required ? <span className={styles.requiredAsterisk}>* </span> : null}
+          {label}
+        </p>
+        <MUITextField
+          ref={ref}
+          fullWidth
+          size="small"
+          className={styles.inputClass}
+          error={error}
+          InputProps={{
+            classes: {
+              input: styles.input,
+            },
+          }}
+          {...props}
+        />
+        {helperText ? <div className={styles.helperText}>{helperText}</div> : null}
+      </div>
+    );
+  },
+);
+TextField.displayName = "TextField";
+export default TextField;
