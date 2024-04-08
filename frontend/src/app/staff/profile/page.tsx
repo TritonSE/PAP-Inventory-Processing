760646a32d59b40b7522d6a19a9717d8ba91e589
sydneyzhang18 <114766656+sydneyzhang18@users.noreply.github.com>
2024-04-08 12:40:51 -0700

    profile page frontend progress

---
+++ frontend/src/app/staff/profile/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import styles from "@/app/staff/profile/page.module.css";
+import VSRTable from "@/components/VSRTable/VSRTable";
+import SearchKeyword from "@/components/VSRTable/SearchKeyword";
+import PageTitle from "@/components/VSRTable/PageTitle";
+import HeaderBar from "@/components/shared/HeaderBar";
+import Image from "next/image";
+import React, { useContext, useEffect, useState } from "react";
+import { StatusDropdown } from "@/components/shared/StatusDropdown";
+import { useMediaQuery } from "@mui/material";
+import { useRedirectToLoginIfNotSignedIn } from "@/hooks/useRedirection";
+import { UserContext } from "@/contexts/userContext";
+import { DeleteVSRsModal } from "@/components/shared/DeleteVSRsModal";
+import { VSR, getAllVSRs } from "@/api/VSRs";
+import { VSRErrorModal } from "@/components/VSRForm/VSRErrorModal";
+import { useScreenSizes } from "@/hooks/useScreenSizes";
+import { LoadingScreen } from "@/components/shared/LoadingScreen";
+import { Button } from "@/components/shared/Button";
+import { UserProfile } from "@/components/UserProfile";
+import { User } from "firebase/auth";
+
+export default function Profile() {
+  const { isMobile, isTablet } = useScreenSizes();
+
+  //   useRedirectToLoginIfNotSignedIn();
+
+  return (
+    <div className={styles.page}>
+      <HeaderBar showLogoutButton />
+      <div className={styles.main}>
+        <h1 className={styles.title}>Account</h1>
+        <h1 className={styles.subtitle}>User Profile</h1>
+        {/* ACCOUNT INFO */}
+
+        <div className={styles.row}>
+          <h1 className={styles.subtitle}>Manage Users</h1>
+          <Button
+            variant="primary"
+            outlined
+            iconPath="/icon_plus.svg"
+            iconAlt="Add"
+            text="Add Account"
+            hideTextOnMobile
+            // onClick={() => setDeleteVsrModalOpen(true)}
+          />
+        </div>
+
+        <UserProfile name="Justin" email="j@gmail.com"></UserProfile>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/UserProfile/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "@/components/UserProfile/styles.module.css";
+import Image from "next/image";
+import { User } from "firebase/auth";
+
+export interface UserProps {
+  name: string;
+  email: string;
+}
+export function UserProfile({ name, email }: UserProps) {
+  return (
+    <div className={styles.user}>
+      {/* <div className={styles.image}> */}
+      <Image
+        className={styles.pfp}
+        src="/Images/LoginImage.png"
+        alt="Internal Error"
+        width={93}
+        height={93}
+      />
+      {/* </div> */}
+      <div className={styles.info}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.email}>{email}</p>
+      </div>
+      <Image
+        src="/ic_settings.svg"
+        alt="Internal Error"
+        // width={isMobile ? 100 : 155}
+        // height={isMobile ? 69 : 106}
+        width={18}
+        height={18}
+      />
+    </div>
+  );
+}
