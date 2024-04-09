cb4f3835be57a1777b1c4f2d4436d79e3c92e434
Daniel <shao.danielw@gmail.com>
2024-04-09 13:01:52 -0700

    added delete api route

--- backend/src/routes/furnitureItem.ts
+++ backend/src/routes/furnitureItem.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { requireAdmin, requireSignedIn, requireStaffOrAdmin } from "src/middleware/auth";
 import * as FurnitureItemController from "src/controllers/furnitureItem";
 
 const router = express.Router();
 
 router.get("/", FurnitureItemController.getFurnitureItems);
+router.delete("/:id", requireSignedIn, requireStaffOrAdmin, FurnitureItemController.deleteFurnitureItem);
+
 
 export default router;
