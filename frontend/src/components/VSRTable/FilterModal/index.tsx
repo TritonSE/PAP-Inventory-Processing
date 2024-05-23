9d904288f2965aee6ca31bdc9eccb200306612c2
HarshGurnani <gurnani.harsh@gmail.com>
2024-05-22 19:37:21 -0700

    modified zip code filtering

--- frontend/src/components/VSRTable/FilterModal/index.tsx
+++ frontend/src/components/VSRTable/FilterModal/index.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/shared/Button";
 import TextField from "@/components/shared/input/TextField";
 import MultipleChoice from "@/components/shared/input/MultipleChoice";
 import { incomeOptions } from "@/constants/fieldOptions";
+import FilterChip from "@/components/VSRTable/FilterChip";
 import { on } from "events";
 
 interface FilterModalProps {
@@ -17,22 +18,49 @@ interface FilterModalProps {
 const FilterModal = ({ isOpen, onClose, onInputEntered, onResetFilters }: FilterModalProps) => {
   if (!isOpen) return null;
 
+  const finalZipCodes: string[] = [];
   const [zipCodes, setZipCodes] = useState<string[]>([]);
+  const [currentZipCode, setCurrentZipCode] = useState("");
+
   const [income, setIncome] = useState<string>("");
 
   const handleZipCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setZipCodes(e.target.value.split(",").map((zip) => zip.trim())); // Assuming zip codes are comma-separated
+    // setZipCodes(e.target.value.split(",").map((zip) => zip.trim())); // Assuming zip codes are comma-separated
+    setCurrentZipCode(e.target.value);
   };
 
   const handleApplyFilter = () => {
     // Pass the entered zip codes to the parent component when the user applies the filter
-    onInputEntered(zipCodes, income);
+    zipCodes.forEach((zipCode) => {
+      finalZipCodes.push(zipCode);
+    });
+    if (currentZipCode.trim() !== "") {
+      finalZipCodes.push(currentZipCode);
+    }
+    onInputEntered(finalZipCodes, income);
     onClose(); // Close the modal
   };
 
   const handleReset = () => {
     onResetFilters();
     onClose(); // Close the modal
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      if (currentZipCode.trim()) {
+        setZipCodes((prevZipCodes) => [...prevZipCodes, currentZipCode.trim()]);
+        setCurrentZipCode(""); // Clear the text field after adding the zipcode
+      }
+      event.preventDefault(); // Prevent the default action of the Enter key
+    }
+  };
+
+  const handleZipCodeEnter = () => {
+    if (currentZipCode.trim()) {
+      setZipCodes((prevZipCodes) => [...prevZipCodes, currentZipCode.trim()]);
+      setCurrentZipCode(""); // Clear the text field after adding the zipcode
+    }
   };
 
   return (
@@ -53,13 +81,36 @@ const FilterModal = ({ isOpen, onClose, onInputEntered, onResetFilters }: Filter
               required
             ></MultipleChoice>
             <br />
-            <TextField
-              label="Zip Code(s)"
-              variant="outlined"
-              placeholder="e.g. 92093"
-              onChange={handleZipCodeChange}
-              required={false}
-            />
+            <div className={styles.zipCodeContainer}>
+              <TextField
+                label="Zip Code(s)"
+                variant="outlined"
+                placeholder="e.g. 92093"
+                onChange={handleZipCodeChange}
+                onKeyDown={handleKeyDown}
+                value={currentZipCode}
+                required={false}
+              />
+              <Button
+                variant="primary"
+                outlined={false}
+                text="Enter"
+                onClick={handleZipCodeEnter}
+                className={styles.enterButton}
+              />
+            </div>
+            <br />
+            <div className={styles.filterChips}>
+              {zipCodes?.map((zipCode) => (
+                <FilterChip
+                  label={"Zip Code: " + zipCode}
+                  key={zipCode}
+                  onDelete={function (): void {
+                    setZipCodes(zipCodes?.filter((z) => z !== zipCode));
+                  }}
+                />
+              ))}
+            </div>
           </div>
         }
         bottomRow={
