84fff9bd875b0776e9203478821176a6102d5eb3
benjaminjohnson2204 <ben332004@gmail.com>
2024-06-01 10:59:27 -0700

    Debug firebase settings env var

--- frontend/src/firebase/firebase.ts
+++ frontend/src/firebase/firebase.ts
@@ -7,9 +7,10 @@ import env from "@/util/validateEnv";
  * environment variable.
  */
 export const initFirebase = () => {
-  if (!env.NEXT_PUBLIC_FIREBASE_SETTINGS) {
-    throw new Error("Cannot get firebase settings");
-  }
+  throw new Error("firebase settings are " + env.NEXT_PUBLIC_FIREBASE_SETTINGS);
+  // if (!env.NEXT_PUBLIC_FIREBASE_SETTINGS) {
+  //   throw new Error("Cannot get firebase settings");
+  // }
 
   const firebaseConfig = env.NEXT_PUBLIC_FIREBASE_SETTINGS;
 
